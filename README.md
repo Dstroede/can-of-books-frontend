158677468d8a731dc0b19c80d4b09286c3b308c2
DrewStroede <dstrow13@gmail.com>
2023-12-09 22:35:42 -0600

    created document

--- README.md
+++ README.md
@@ -1,8 +1,67 @@
-# React + Vite
+# Can-of-Books
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+**Author**: Drew Stroede
+**Version**: 1.0.0
 
-Currently, two official plugins are available:
+## Overview
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+This application showcases a curated selection of my preferred literary works while highlighting my proficiency in utilizing MongoDB, React, JavaScript, and Bootstrap.
+
+## Getting Started
+
+
+## Architecture
+
+As stated previously, this app features MongoDB, React, Javascript, Bootstrap, and Vite.
+
+## Lighthouse
+
+![Lighthouse Score](src/photos/Lighthouse.png)
+
+## Change Log
+
+Started Dec 2, 2023
+
+## Credit and Collaborations
+
+Credit to Code Fellows
+
+## Name of feature: App creation
+
+Estimate of time needed to complete: 2 0min
+
+Start time: 2:45pm
+
+Finish time: 3:15pm
+
+Actual time needed to complete: 30 min
+
+## Name of feature: Adding Carosouel
+
+Estimate of time needed to complete: 1hr
+
+Start time: 4:05pm
+
+Finish time: 4:35pm
+
+Actual time needed to complete: 30 min
+
+## Name of feature: Set Route
+
+Estimate of time needed to complete: 45 min
+
+Start time: 8:30am
+
+Finish time: 8:45am
+
+Actual time needed to complete: 15 min
+
+## Name of feature: Adding About Me
+
+Estimate of time needed to complete: 20 min
+
+Start time: 11:30pm
+
+Finish time: 12:45pm
+
+Actual time needed to complete: 1 hr 15 min
--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,23 +11,55 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
-
+async componentDidMount(){
+    try {
+        let foundBooks = await fetch(`${import.meta.env.VITE_SERVER_URL}/books`);
+        if (foundBooks.ok) {
+            let bookData= await foundBooks.json();
+            this.setState({
+                books: bookData
+            });
+            console.log('Found these books:', bookData)
+        }else {
+            console.error('No Books Found. Server response:', res.status )
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
   render() {
+    let carouselStyle = {
+        margin: '2rem',
+    }
 
-    /* TODO: render all the books in a Carousel */
+        return (
+            <>
+                <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    )
-  }
+                {this.state.books.length > 0 ? (
+                    <Carousel style= {carouselStyle} data-bs-theme="dark">
+                        {this.state.books.map((book, index) => (
+                            <Carousel.Item key={index}>
+                                {book.img ? (
+                                    <img src={book.img} alt={`Slide ${index + 1}`} />
+                                ) : (
+                                    // Use a placeholder image when img is not available
+                                    <img src="https://fakeimg.pl/600x500/FFFFFF/FFFFFF" alt={`Slide ${index + 1}`} />
+                                )}
+                                <Carousel.Caption className="book-card">
+                                    <h3>{book.title}</h3>
+                                    <p>{book.description}</p>
+                                    <p>{book.status}</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                ) : (
+                    <h3>No Books Found</h3>
+                )}
+            </>
+        );
+    }
 }
 
 export default BestBooks;
